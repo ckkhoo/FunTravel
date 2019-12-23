823502d270fc6c0934a70d6fb86cb31310a64484
absoluteck <45701871+absoluteck@users.noreply.github.com>
2019-12-23 22:08:10 +0800

    Initial

---
+++ app/src/main/java/com/example/localguide/signin.kt
@@ -0,0 +1,12 @@
+package com.example.localguide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class signin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
